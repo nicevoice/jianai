cecedc2bebda8522be7319a93d153267e7f1d189
Roy Guo <royguo1988@gmail.com>
2012-07-17 21:48:20 +0800

    #

--- db/commons.php
+++ db/commons.php
@@ -1,5 +1,8 @@
 <?php
 /*
+ * @author royguo1988@gmail.com
+ * @date 2012-07-15
+ * 
  * Created on Jul 13, 2012
  * 数据库操作的公共函数
  */
--- db/men.php
+++ db/men.php
@@ -1,5 +1,8 @@
 <?php
-/*
+/* 
+ * @author royguo1988@gmail.com
+ * @date 2012-07-15
+ * 
  * Created on Jul 13, 2012
  * 男士产品的数据库查询函数集合
  */
